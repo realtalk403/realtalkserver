29023e041199eff8c31461d3fdee76d7ff2cd405
Colin Kho(HP) <colinzkho@hotmail.com>
2013-04-29 18:42:35 -0700

    Added RegistrationIdServlet.java * Handles a change of registration id for a user
    Modified RequestParameters.java
    * Added string constant used to retrieve new Registration ID
    
    Modified web.xml
    * Set Url pattern for RegistrationIdServlet.java
    
    Modified UserManager.java
    * Modified for debugging purposes.

---
+++ src/main/java/com/realtalkserver/servlet/RegistrationIdServlet.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.realtalkserver.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.realtalkserver.util.RequestParameters;
+import com.realtalkserver.util.UserManager;
+
+/**
+ * Servlet that handles a request to change the registration ID of a user. It
+ * requires a password for authentication purposes.
+ * 
+ * @author Colin Kho
+ *
+ */
+@SuppressWarnings("serial")
+public class RegistrationIdServlet extends BaseServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// Retrieve the parameter information.
+        String stRegId = getParameter(req, RequestParameters.PARAMETER_REG_ID);
+        String stUser = getParameter(req, RequestParameters.PARAMETER_USER);
+        String stPwd = getParameter(req, RequestParameters.PARAMETER_PWORD);
+        
+        String stNewRegId = getParameter(req, RequestParameters.PARAMETER_NEW_REG_ID);
+        
+        boolean fChangeIdSuccess = UserManager.fChangeId(stUser, stPwd, stRegId, stNewRegId);
+        
+        // Generate JSON Response
+        JSONObject jsonResponse = new JSONObject();
+        try {
+        	String stSuccessMsg = fChangeIdSuccess ? "true" : "false";
+        	jsonResponse.put(RequestParameters.PARAMETER_SUCCESS, stSuccessMsg);	
+        	jsonResponse.put(RequestParameters.PARAMETER_USER, stUser);
+        	jsonResponse.put(RequestParameters.PARAMETER_REG_ID, stRegId);
+        	jsonResponse.put(RequestParameters.PARAMETER_PWORD, stPwd);
+        	jsonResponse.put(RequestParameters.PARAMETER_NEW_REG_ID, stNewRegId);
+        } catch (JSONException e) {
+        	// Exception will never be throw because keys are not null
+        }
+        
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        out.write(jsonResponse.toString());
+        setSuccess(resp);
+	}
+}
--- src/main/java/com/realtalkserver/util/RequestParameters.java
+++ src/main/java/com/realtalkserver/util/RequestParameters.java
@@ -19,6 +19,8 @@ public class RequestParameters {
     public static final String PARAMETER_PWORD = "PARAMETER_PWORD";
     // New Password - Used if user wants to change password
     public static final String PARAMETER_NEW_PWORD = "PARAMETER_NEW_PWORD";
+    // New Registration ID for GCM - Used if ID is changed
+    public static final String PARAMETER_NEW_REG_ID = "PARAMETER_NEW_REG_ID";
     // Parameter that indicates success - return "true" or "false" for this.
     public static final String PARAMETER_SUCCESS = "success";
 }
