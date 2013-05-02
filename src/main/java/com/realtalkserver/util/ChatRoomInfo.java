15b264b836ed11cc395355bdcb9fd2a0e94d2e85
Colin Kho(HP) <colinzkho@hotmail.com>
2013-05-01 17:06:46 -0700

    Added ResponseParameters.java for error codes and responses. Added PostMessageServlet.java. Updated core files. Purpose for check-in: current collaboration with server team

--- src/main/java/com/realtalkserver/util/ChatRoomInfo.java
+++ src/main/java/com/realtalkserver/util/ChatRoomInfo.java
@@ -3,6 +3,8 @@
  */
 package com.realtalkserver.util;
 
+import java.sql.Timestamp;
+
 /**
  * ChatRoomInfo is an immutable container class used to represent a chat room.
  * 
@@ -15,8 +17,7 @@ public class ChatRoomInfo {
     private String location; //TODO : May want to be coordinates
     private String creator;
     private int numUsers;
-    private String dateCreated;
-    private String timeCreated;
+    private Timestamp timeStampCreated;
     
     /**
      * @param name         Chat Room Name
@@ -24,18 +25,36 @@ public class ChatRoomInfo {
      * @param location     Chat Room Location
      * @param creator      Chat Room Creator
      * @param numUsers     Number of users in chat room
-     * @param dateCreated  Date Chat Room created
-     * @param timeCreated  Time Chat Room created
+     * @param timeStamp    Timestamp of when room was created.
      */
     public ChatRoomInfo(String name, String id, String location,
-            String creator, int numUsers, String dateCreated, String timeCreated) {
+            String creator, int numUsers, Timestamp timeStampCreated) {
         this.name = name;
         this.id = id;
         this.location = location;
         this.creator = creator;
         this.numUsers = numUsers;
-        this.dateCreated = dateCreated;
-        this.timeCreated = timeCreated;
+        this.timeStampCreated = timeStampCreated;
+    }
+    
+    /**
+     * @param name         Chat Room Name
+     * @param id           Chat Room Id
+     * @param location     Chat Room Location
+     * @param creator      Chat Room Creator
+     * @param numUsers     Number of users in chat room
+     * @param timeStamp    Timestamp of when room was created in the
+     *                     form of a long where it is the milliseconds 
+     *                     since January 1, 1970, 00:00:00 GMT
+     */
+    public ChatRoomInfo(String name, String id, String location,
+            String creator, int numUsers, long timeStampCreated) {
+        this.name = name;
+        this.id = id;
+        this.location = location;
+        this.creator = creator;
+        this.numUsers = numUsers;
+        this.timeStampCreated = new Timestamp(timeStampCreated);
     }
 
     /**
@@ -73,17 +92,11 @@ public class ChatRoomInfo {
         return numUsers;
     }
 
-    /**
-     * @return the dateCreated
-     */
-    public String getDateCreated() {
-        return dateCreated;
-    }
+	/**
+	 * @return the timeStampCreated
+	 */
+	public Timestamp getTimeStampCreated() {
+		return new Timestamp(timeStampCreated.getTime());
+	}
 
-    /**
-     * @return the timeCreated
-     */
-    public String getTimeCreated() {
-        return timeCreated;
-    }
 }
--- src/main/java/com/realtalkserver/util/RequestParameters.java
+++ src/main/java/com/realtalkserver/util/RequestParameters.java
@@ -23,7 +23,14 @@ public class RequestParameters {
     public static final String PARAMETER_NEW_REG_ID = "PARAMETER_NEW_REG_ID";
     // Parameter that indicates success - return "true" or "false" for this.
     public static final String PARAMETER_SUCCESS = "success";
-    
-    public static final String PARAMETER_ROOM_NAME = "PARAMETER_REG_ID";
-    public static final String PARAMETER_ROOM_ID = "PARAMETER_REG_ID";
+    // Room Name
+    public static final String PARAMETER_ROOM_NAME = "PARAMETER_ROOM_NAME";
+    // Room ID
+    public static final String PARAMETER_ROOM_ID = "PARAMETER_ROOM_ID";
+    // Message Body
+    public static final String PARAMETER_MESSAGE_BODY = "PARAMETER_MESSAGE_BODY";
+    // Message Sender
+    public static final String PARAMETER_MESSAGE_SENDER = "PARAMETER_MESSAGE_SENDER";
+    // Message Timestamp
+    public static final String PARAMETER_MESSAGE_TIMESTAMP = "PARAMETER_MESSAGE_TIMESTAMP";
 }
